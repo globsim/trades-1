b7276e0d039952169eae43b22619984cef8be2d4
amos baranes <amos@drbaranes.com>
2019-04-27 20:58:34 +0300

    Amos initial commit4

--- web/trades/apps/todo/views.py
+++ web/trades/apps/todo/views.py
@@ -9,17 +9,8 @@ except Exception:
 
 
 def index(request):
-    print('--------')
-    print(request.method)
-    print('--------')
-
     if request.method == 'POST':
-        print('---2-----')
-        print(request.POST['item_text'])
         Item.objects.create(text=request.POST['item_text'])
-
-        print('---21-----')
-
         # return redirect('/')
     items = Item.objects.all()
     counter = 100
@@ -27,7 +18,4 @@ def index(request):
         counter = redis.incr('counter')
     except Exception:
         pass
-    print('--------')
-    print(1)
-    print('--------')
     return render(request, 'todo/index.html', {'items': items, 'counter': counter})
